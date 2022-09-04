fec04451d6a913866bd341019301de8197498f00
iarav <iarav@192.168.0.13>
2022-09-03 23:55:07 -0300

    fiz umas coisas enquanto testava

---
+++ src/main/java/com/mycompany/digraph/Controller.java
@@ -0,0 +1,27 @@
+package com.mycompany.digraph;
+
+import java.io.IOException;
+
+/**Controller Class
+* @author Iara
+* @version 1.0
+*/
+public class Controller {
+    
+    /**Methods*/
+    
+    /**Empty Constructor*/
+    public Controller() {
+    }
+    
+    /**Method that calls TextConversion object and its method
+     * @throws java.io.IOException*/
+    public void createTextConversion() throws IOException{
+        TextConversion textConversion1;
+        textConversion1 = new TextConversion();
+        String pathScanned = textConversion1.readPathFile();
+        textConversion1.setPathFile(pathScanned);
+        textConversion1.readFile();
+    }
+    
+}
