41018a14ebf5350a6895962768aeedc6460bc500
iarav <iarav@192.168.0.13>
2022-09-05 11:42:35 -0300

    mudei o nome da classe para o certo e add a FileTransform

---
+++ src/main/java/com/mycompany/digraph/InitProgram.java
@@ -0,0 +1,16 @@
+package com.mycompany.digraph;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class InitProgram {
+
+    public static void main(String[] args) throws IOException {
+        Controller controller1;
+        controller1 = new Controller();
+        controller1.createTextConversion();
+    }
+}
