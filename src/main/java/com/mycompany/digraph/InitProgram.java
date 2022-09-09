4ea3c9cd48787f904b2023212ff5d8110d74eb5d
Brenda Juliane <bre8juliane@hotmail.com>
2022-09-09 12:45:56 -0300

    Utilizando o metodo da classe de controle como estático

--- src/main/java/com/mycompany/digraph/InitProgram.java
+++ src/main/java/com/mycompany/digraph/InitProgram.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 
 /**
  *
- * @author LENOVO
+ * @author Iara
  */
 public class InitProgram {
 
     public static void main(String[] args) throws IOException {
-        Controller controller1;
-        controller1 = new Controller();
-        controller1.createTextConversion();
+        Controller.createTextConversion();
     }
 }
