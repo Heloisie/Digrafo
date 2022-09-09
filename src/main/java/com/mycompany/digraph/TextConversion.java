38fb6e0332c14da0149a1e9336a9fdf757e9eb5f
Brenda Juliane <bre8juliane@hotmail.com>
2022-09-09 12:50:19 -0300

    Exclusão do atributo, métodos agora são estáticos e mudança no método 'readPathFile'

--- src/main/java/com/mycompany/digraph/TextConversion.java
+++ src/main/java/com/mycompany/digraph/TextConversion.java
@@ -8,52 +8,39 @@ import java.util.ArrayList;
 
 /**Class to the conversion of the text in the file
 * @author Iara
+* @author Brenda
 * @version 1.0
 */
 
 public class TextConversion {
-    /**Atributes*/
-    private String pathFile;
-
     /**Methods*/
     
-    /**Get Method
-    * @return String - pathFile*/
-    public String getPathFile() {
-        return pathFile;
-    }
-
-    /**Set Method
-    * @param pathFile String - path of the choosen file*/
-    public void setPathFile(String pathFile) {
-        this.pathFile = pathFile;
-    }
-    
-    /**Constructor
-    * @param pathFile String - path of the choosen file*/
-    public TextConversion(String pathFile) {
-        this.pathFile = pathFile;
-    }
-    
     /**Empty Constructor*/
-    public TextConversion() {
-    }
+    public TextConversion() {}
     
     /**Method that read the path to the file that is going to convert and return it
-    * @return String - pathScanned*/
-    public String readPathFile() {
-        System.out.println("Digite o caminho: ");
+    * @return String - pathScanned
+    */
+    public static ArrayList<String> readPathFile() {
+        System.out.println("Enter the files separated by ',': ");
         /**Using Scanner for Getting Input from User*/
         Scanner in = new Scanner(System.in);
- 
-        String pathScanned = in.nextLine();
+        ArrayList<String> pathScanned = new ArrayList<>();
+        String[] list;
+        
+        String text = in.next();
+        list = text.split(",");      
+        for (int i = 0; i < list.length; i++) {
+			pathScanned.add(list[i]);
+		}
         return pathScanned;
     }
     
     /**Method that format the String parameter
     * @param value String - sting to be formated
-    * @return String - StringWords*/
-    public String[] textFormat(String value) {
+    * @return String - StringWords
+    */
+    public static String[] textFormat(String value) {
         value = value.toLowerCase(); /**transform all string in lower case*/
         value = value.replaceAll("\\p{Punct}", ""); /**remove all punctuation*/
         
@@ -64,10 +51,11 @@ public class TextConversion {
     
     /**Method that read a file and return an ArrayList with all the formated words
     * @return String - FileWords
-    * @throws java.io.IOException*/
-    public ArrayList<String> readFile() throws IOException {
+    * @throws java.io.IOException
+    */
+    public static ArrayList<String> readFile(String pathFileS) throws IOException {
         ArrayList<String> FileWords = new ArrayList<>();
-        try (BufferedReader buffRead = new BufferedReader(new FileReader(this.pathFile))) {
+        try (BufferedReader buffRead = new BufferedReader(new FileReader(pathFileS))) {
             String linha = "";
             while (true) {
                 if (linha != null) {
